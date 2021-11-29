0a531f1cbf660b8b7a2aece131ee20860a1d92f5
Ali Girayhan Ozbay <aligirayhan.ozbay14@imperial.ac.uk>
2021-11-29 10:38:49 +0000

    moved rnn version to different file

--- flow_prediction/models/RNN_TemporalUNet.py
+++ flow_prediction/models/RNN_TemporalUNet.py
@@ -107,38 +107,17 @@ class RNN_TemporalUNet(tf.keras.models.Model):
 
         super().__init__(rnn_keras_tensors[0], unet_final_layer)
 
-class SD_TemporalUNet(tf.keras.models.Model):
-    def __init__(self, input_units, grid_shape, out_channels = 1, sd_config = None, temporalunet_config = None, nt = None):
-        
-        if sd_config is None:
-            sd_config = {}
-        sd_final_units = int(np.prod(grid_shape))
-        shallow_decoder_layers = _get_original_shallow_decoder_layers([nt,input_units], sd_final_units, **sd_config)
-        shallow_decoder_keras_tensors = convert_to_keras_tensors(shallow_decoder_layers)
-        
-        unet_input_shape = [nt,1,*grid_shape] if tf.keras.backend.image_data_format() == 'channels_first' else [nt,*grid_shape,1]
-        unet_input_reshape_layer = tf.keras.layers.Reshape(unet_input_shape)(shallow_decoder_keras_tensors[-1])
-
-        if temporalunet_config is None:
-            temporalunet_config = {}
-        unet_final_layer = TemporalUNet(input_layer = unet_input_reshape_layer, nt = None,  nx = None, ny = None, in_channels=None, out_channels=out_channels, padding='same', return_layer = True, **temporalunet_config)
-
-        super().__init__(shallow_decoder_keras_tensors[0], unet_final_layer)
-
-
 if __name__=='__main__':
     
     tf.keras.backend.set_image_data_format('channels_first')
 
 
     nf = 16
-    nx = 64
-    ny = 256
+    nx = 32
+    ny = 128
     nt = 10
     nc = 2
     bsize = 5
     inp = tf.random.uniform((bsize,nt,nf))
 
-    #model = RNN_TemporalUNet(nf, [nx, ny], out_channels = nc, nt = nt)
-    model = SD_TemporalUNet(nf, [nx, ny], out_channels = nc, nt = nt)
-    model.summary()
+    model = RNN_TemporalUNet(nf, [nx, ny], out_channels = nc, nt = nt)
