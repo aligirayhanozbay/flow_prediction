3240d24669ba10886323ce5d036af35bcc89d531
Ali Girayhan Ozbay <giray@snorlax.broadband>
2022-06-13 12:19:58 +0100

    retry with another geom if meshing fails

--- flow_prediction/data/geometry/gen_Omesh.py
+++ flow_prediction/data/geometry/gen_Omesh.py
@@ -14,22 +14,29 @@ args = parser.parse_args()
 cfg = json.load(open(args.bezier_cfg,'r'))
 
 for k in range(args.n):
-    if args.m:
-        max_retries = 10
-        cur_tries = 0
-        mapping_quality = (math.inf,math.inf)
-        while ((mapping_quality[0] > args.m) or (mapping_quality[1] > args.m)) and (cur_tries<max_retries):
-            shape = Shape(**cfg['shape'])
-            mapping_quality = shape.generate(**cfg['generate'], check_mapping=True)
-            cur_tries += 1
-            if args.v:
-                print(f'Shape {k} - generation attempt {cur_tries}: quality {mapping_quality} | threshold {args.m}')
-        if cur_tries == max_retries and ((mapping_quality[0] > args.m) or (mapping_quality[1] > args.m)):
-            raise(RuntimeError('Reached max # of shape generation attempts.'))
-    else:
-        shape = Shape(**cfg['shape'])
-        mapping_quality = shape.generate(**cfg['generate'], check_mapping=False)
+    success = False
+    while not success:
+        try:
+            if args.m:
+                max_retries = 10
+                cur_tries = 0
+                mapping_quality = (math.inf,math.inf)
+                while ((mapping_quality[0] > args.m) or (mapping_quality[1] > args.m)) and (cur_tries<max_retries):
+                    shape = Shape(**cfg['shape'])
+                    mapping_quality = shape.generate(**cfg['generate'], check_mapping=True)
+                    cur_tries += 1
+                    if args.v:
+                        print(f'Shape {k} - generation attempt {cur_tries}: quality {mapping_quality} | threshold {args.m}')
+                if cur_tries == max_retries and ((mapping_quality[0] > args.m) or (mapping_quality[1] > args.m)):
+                    raise(RuntimeError('Reached max # of shape generation attempts.'))
+            else:
+                shape = Shape(**cfg['shape'])
+                mapping_quality = shape.generate(**cfg['generate'], check_mapping=False)
 
-    fname, n_el = shape.OMesh(**cfg['mesh'], output_dir=args.o)
+            fname, n_el = shape.OMesh(**cfg['mesh'], output_dir=args.o)
+            success = True
+        except:
+            pass
+    
     #print(fname)
     #print(f'n elements: {n_el}')
